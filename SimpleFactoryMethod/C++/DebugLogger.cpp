9ef405d1e34a9b28ef9762ca4e6d926cb819b37e
aakanshabishnoi <82051164+aakanshabishnoi@users.noreply.github.com>
2025-04-05 11:32:55 +0530

    Delete SimpleFactoryMethod directory

--- SimpleFactoryMethod/C++/DebugLogger.cpp
+++
@@ -1,7 +0,0 @@
-#include "DebugLogger.hpp"
-using namespace std;
-
-void DebugLogger::log(const string& msg)
-{
-    cout<< "DEBUG: " << msg<< endl;
-}
--- SimpleFactoryMethod/C++/ErrorLogger.cpp
+++
@@ -1,6 +0,0 @@
-#include "ErrorLogger.hpp"
-using namespace std;
-
-void ErrorLogger::log(const string& msg) {
-    cout<<"ERROR: " << msg << endl;
-}
